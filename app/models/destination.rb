8f24a9925713bd4138d00a19d963ec3d2a3727bd
“Abbie <abbie.coghlan@gmail.com>
2021-01-21 14:45:36 -0600

    saving

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,9 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
 
+    def bloggers_average_age
+        ages = self.bloggers.collect {|b| b.age.to_f}
+        ages.sum / ages.count 
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+    belongs_to :destination
+    belongs_to :blogger
+
+    validates :content, length: {minimum: 100}
 
 end
